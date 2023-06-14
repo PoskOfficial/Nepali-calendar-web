453e5868bda7242de5504792612a3115fff21b91
Parajulibkrm <bikramparajuli0000@gmail.com>
2023-06-14 19:38:25 +0545

    chore: :bug: Fixed PWA issue

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,29 @@ export default defineConfig({
         background_color: "#ffffff",
         orientation: "portrait-primary",
         categories: ["utility", "tools", "productivity"],
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
       devOptions: {
         enabled: true,
