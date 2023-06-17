7c75eaec91b4d5caee8ff55326cde161d602a901
Parajulibkrm <bikramparajuli0000@gmail.com>
2023-06-17 18:43:01 +0545

    Added Sign In

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { checkIfUserIsLoggedInOrOffline } from "../helper/api";
 
 const navigation = [
   { name: "Home", href: "/", current: true },
@@ -14,6 +15,21 @@ function classNames(...classes: string[]): string {
 }
 
 export default function Navbar() {
+  const [status, setStatus] = useState<"OFFLINE" | "NOT_LOGGED_IN" | "LOGGED_IN">("OFFLINE");
+
+  const [photoUrl, setPhotoUrl] = useState(null);
+
+  const checkStatus = async () => {
+    const resp = await checkIfUserIsLoggedInOrOffline();
+    setStatus(resp.status);
+    if (resp.status === "LOGGED_IN") {
+      setPhotoUrl(resp.data.user._json.picture);
+    }
+  };
+  useEffect(() => {
+    checkStatus();
+  }, []);
+
   return (
     <Disclosure as="nav" className="border-b bg-white">
       {({ open }) => (
@@ -55,41 +71,54 @@ export default function Navbar() {
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95">
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700"
-                            )}>
-                            Sign out
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                {status === "LOGGED_IN" ? (
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src={
+                            photoUrl ||
+                            "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png"
+                          }
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95">
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}>
+                              Sign out
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                ) : status === "NOT_LOGGED_IN" ? (
+                  <a
+                    href="/api/auth/google"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-900">
+                    Login
+                  </a>
+                ) : (
+                  "offline"
+                )}
               </div>
             </div>
           </div>
--- src/helper/api.ts
+++ src/helper/api.ts
@@ -3,3 +3,18 @@ export async function fetchYearlyData(year: string) {
   const data = await res.json();
   return data;
 }
+export async function checkIfUserIsLoggedInOrOffline(): Promise<{
+  status: "LOGGED_IN" | "NOT_LOGGED_IN" | "OFFLINE";
+  data?: any;
+}> {
+  const res = await fetch("/api/profile");
+  // console.log({ res });
+  if (res.status === 200) {
+    const data = await res.json();
+    return { status: "LOGGED_IN", data };
+  }
+  if (res.status === 401) {
+    return { status: "NOT_LOGGED_IN" };
+  }
+  return { status: "OFFLINE" };
+}
