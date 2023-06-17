ee1a8218c25430262dd167d5489a3a7a407730da
nirajacharyaa <mail.nirajacharya@gmail.com>
2023-06-17 14:13:18 +0545

    feat: :sparkles: user events modal and view events

--- src/UpcomingEvents.tsx
+++ src/UpcomingEvents.tsx
@@ -1,19 +1,21 @@
 import SingleEvent from "./components/SingleEvent";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import { Day, YearData } from "./types";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useEffect } from "react";
 import NepaliDate from "nepali-date-converter";
 import { fetchYearlyData } from "./helper/api";
-import { getNepaliMonth } from "./helper/dates";
-import nepaliNumber from "./helper/nepaliNumber";
-function UpcomintEvents() {
+import DropDown from "./components/DropDown";
+import availableYears from "./constants/availableYears";
+import { nepaliMonths } from "./constants/mahina";
+function UpcomingEvents() {
   function classNames(...classes: Array<string | undefined | boolean>) {
     return classes.filter(Boolean).join(" ");
   }
   const today = new NepaliDate(); //gets today's nepali date
   const [activeYear, setActiveYear] = useState(today.getYear());
-  const [nepaliMonthNumber, setNepaliMonthNumber] = useState(today.getMonth() + 1);
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth());
+  // const [nepaliMonthNumber] = useState(today.getMonth() + 1);
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState({} as YearData);
   useEffect(() => {
@@ -26,63 +28,76 @@ function UpcomintEvents() {
     getFile();
   }, [activeYear]);
 
+  const handleNextMonth = () => {
+    if (currentMonth == 11) {
+      setActiveYear((prev: number) => prev + 1);
+      setCurrentMonth((prev: number) => prev % 11);
+    } else {
+      setCurrentMonth((prev) => prev + 1);
+    }
+  };
+
+  const handlePrevMonth = () => {
+    if (currentMonth == 0) {
+      setActiveYear((prev: number) => prev - 1);
+      setCurrentMonth(11);
+    } else {
+      setCurrentMonth((prev) => prev - 1);
+    }
+  };
+
+  const monthData = useMemo(() => {
+    return data[currentMonth > 9 ? (currentMonth + 1).toString() : `0${currentMonth + 1}`];
+  }, [data, currentMonth]);
+  console.log(currentMonth > 9 ? currentMonth.toString() : `0${currentMonth}`);
+
   if (loading || !data) {
     return <></>;
   }
   return (
-    <div className="lg:1/2 mx-auto   md:w-2/3">
-      <div className="flex max-w-[600px] items-center py-4 text-center  text-gray-900">
+    <div className="lg:1/2 mx-auto px-4 md:w-2/3">
+      <div className="flex items-center text-gray-900">
         <button
-          onClick={() => {
-            if (nepaliMonthNumber > 1) setNepaliMonthNumber(nepaliMonthNumber - 1);
-            else if (activeYear > 2075) {
-              setNepaliMonthNumber(12);
-              setActiveYear(activeYear - 1);
-            }
-          }}
           type="button"
+          disabled={currentMonth === 0 && activeYear === availableYears[0]}
           className={classNames(
-            " flex flex-none items-center justify-center rounded-lg  bg-indigo-600 p-1.5 text-white hover:bg-indigo-700  "
-          )}>
+            " flex flex-none items-center justify-center rounded-lg  bg-indigo-600 p-1.5 text-white hover:bg-indigo-700  disabled:cursor-not-allowed disabled:bg-blue-600 disabled:text-white disabled:opacity-20 disabled:hover:cursor-not-allowed disabled:hover:bg-blue-600 disabled:hover:text-white"
+          )}
+          onClick={handlePrevMonth}>
           <span className="sr-only">Previous month</span>
           <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
         </button>
-        <div className="flex-auto font-mukta font-semibold">
-          {`${getNepaliMonth(nepaliMonthNumber - 1)} ${nepaliNumber(activeYear.toString())}`}
+        <div className="flex w-96 flex-auto items-center justify-center gap-4 font-mukta font-semibold">
+          <DropDown selected={activeYear} setSelected={setActiveYear} items={availableYears} isValue />
+          <DropDown selected={currentMonth} setSelected={setCurrentMonth} items={nepaliMonths} />
         </div>
         <button
-          onClick={() => {
-            if (nepaliMonthNumber < 12) setNepaliMonthNumber(nepaliMonthNumber + 1);
-            else if (activeYear < 2082) {
-              setNepaliMonthNumber(1);
-              setActiveYear(activeYear + 1);
-            }
-          }}
+          type="button"
+          disabled={currentMonth === 11 && activeYear === availableYears[availableYears.length - 1]}
           className={classNames(
-            " flex flex-none items-center justify-center rounded-lg  bg-indigo-600 p-1.5 text-white hover:bg-indigo-700  "
+            " flex flex-none items-center justify-center rounded-lg  bg-indigo-600 p-1.5 text-white hover:bg-indigo-700 disabled:cursor-not-allowed disabled:bg-blue-600 disabled:text-white disabled:opacity-20 disabled:hover:cursor-not-allowed disabled:hover:bg-blue-600 "
           )}
-          type="button">
+          onClick={handleNextMonth}>
           <span className="sr-only">Next month</span>
           <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
         </button>
       </div>
 
-      {data &&
-        data[nepaliMonthNumber < 10 ? `0${nepaliMonthNumber}` : `${nepaliMonthNumber}`].map((day: Day) => {
-          return (
-            day.events?.length > 0 && (
-              <SingleEvent
-                key={day.ad}
-                nepaliDate={day.AD_date.bs}
-                englishDate={day.AD_date.ad}
-                week_day={day.week_day}
-                events={day.events}
-              />
-            )
-          );
-        })}
+      {monthData?.map((day: Day) => {
+        return (
+          day.events?.length > 0 && (
+            <SingleEvent
+              key={day.ad}
+              nepaliDate={day.AD_date.bs}
+              englishDate={day.AD_date.ad}
+              week_day={day.week_day}
+              events={day.events}
+            />
+          )
+        );
+      })}
     </div>
   );
 }
 
-export default UpcomintEvents;
+export default UpcomingEvents;
