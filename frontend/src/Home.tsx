1c633437f723879ce27f1aa6539565d8bd8b3bbb
headshigh <nischalgautam7200@gmail.com>
2023-06-28 21:18:57 +0545

    feat: :construction: added uselangauge hook,

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -4,16 +4,22 @@ import { fetchYearlyData } from "./helper/api";
 import { getCurrentYear } from "./helper/dates";
 import { Day } from "./types";
 import { fetchYearlyDataHelper } from "./constants/availableYears";
-
+import UseLanguage from "./components/useLanguage";
+import nepaliNumber from "./helper/nepaliNumber";
 function Home() {
+  const { isNepaliLanguage } = UseLanguage();
   const [yearData, setYearData] = useState<{ [key: string]: Day[] } | null>(null);
+  const currYear = isNepaliLanguage ? nepaliNumber(getCurrentYear().toString()) : getCurrentYear().toString();
+  console.log(currYear);
   const [currentYear, setCurrentYear] = useState<number>(getCurrentYear());
+  console.log(currentYear);
   async function fetchCalendarData(currentYear: number) {
     const data = await fetchYearlyData(currentYear.toString());
     setYearData(data);
   }
   useEffect(() => {
     fetchCalendarData(fetchYearlyDataHelper(currentYear.toString()));
+    //fetchyeardatahelper function converts year in nepalinumber to english numbers so currentYear can be in any language
   }, [currentYear]);
 
   return (
--- frontend/src/components/UseLanguage.tsx
+++ frontend/src/components/UseLanguage.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 
 function UseLanguage() {
   const { i18n, t } = useTranslation();
+  // if the current selected language is nepali or not
   const isNepaliLanguage = i18n.language != "en-US";
   return { i18n, t, isNepaliLanguage };
 }
