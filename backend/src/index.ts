9db40b8cbdb30e2d7657b5f7d711b5613b6aeaf2
Parajulibkrm <bikramparajuli0000@gmail.com>
2023-06-28 22:05:58 +0545

    fix: :sparkles: Increased cookie life

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
     },
     store: new DetaSessionStore(),
   })
