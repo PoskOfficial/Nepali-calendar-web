778ddfa712f7c417dc8d064657c9c7bf862a88b1
Bikram Parajuli <bikramparajuli0000@gmail.com>
2023-06-30 21:34:02 +0545

    Added Dark Mode Strategy to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,16 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        modalbg: "rgba(0, 0, 0, .5)",
-        hovercolor: "rgba(125, 133, 144, 0.1) ",
-      },
-    },
     fontFamily: {
       sans: ["Inter", "sans-serif"],
       mukta: ["Mukta", "sans-serif"],
     },
   },
+  darkMode: "class",
   plugins: [],
 };
