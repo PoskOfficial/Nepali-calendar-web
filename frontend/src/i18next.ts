8a8ca2a581e3a6f60328d405c03829912f85ab10
headshigh <nischalgautam7200@gmail.com>
2023-07-01 08:56:50 +0545

    js to ts file

--- frontend/src/i18next.ts
+++ frontend/src/i18next.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-const languages = ["en-US", "ne-NP"];
+// const languages = ["en-US", "ne-NP"];
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -10,21 +10,9 @@ i18n
   .init({
     fallbackLng: "en-US",
     debug: true,
-    whitelist: languages,
+    // whitelist: languages,
     interpolation: {
       escapeValue: false, // not needed for react!!
-    },
-
-    // react i18next special options (optional)
-    // override if needed - omit if ok with defaults
-
-    react: {
-      bindI18n: "languageChanged",
-      bindI18nStore: "",
-      transEmptyNodeValue: "",
-      transSupportBasicHtmlNodes: true,
-      transKeepBasicHtmlNodesFor: ["br", "strong", "i"],
-      useSuspense: true,
     },
   });
 
