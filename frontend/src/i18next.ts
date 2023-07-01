fc5f99252becbc94ca76be64befba104bff5bef1
headshigh <nischalgautam7200@gmail.com>
2023-07-01 09:02:35 +0545

    changes in translation files

--- frontend/src/i18next.ts
+++ frontend/src/i18next.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-// const languages = ["en-US", "ne-NP"];
+const languages = ["en-US", "ne-NP"];
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -14,6 +14,7 @@ i18n
     interpolation: {
       escapeValue: false, // not needed for react!!
     },
+    supportedLngs: languages,
   });
 
 export default i18n;
